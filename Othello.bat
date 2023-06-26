90ee1cb176e1ba9a7e6e082b50ffb416e98366f5
simon <faugieres.lolilolo@gmail.com>
2023-06-26 23:08:41 +0200

    Correction affichage toute taille écran

--- Othello.bat
+++ Othello.bat
@@ -1,7 +1,7 @@
 cd SAE201\application-ihm\bin
 
 ::Si votre installation de JDK-20 le nécessite et/ou est à la racine du projet, définissez le path ci-dessous
-::set path=..\..\..\jdk-20\bin
+set path=..\..\..\jdk-20\bin
 
 ::JavaFX (SDK20) doit être à la racine du projet avec le chemin ci-dessous
 java --module-path ..\..\..\javafx-sdk-20\lib --add-modules javafx.controls,javafx.fxml application.Othello
